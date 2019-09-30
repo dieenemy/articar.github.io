c0e712f1a16c12aef18912b273d17e3f0d5882f6
Trần Xuân Tùng <dieenemy1996@gmail.com>
2019-09-30 15:37:43 +0700

    push all

---
+++ 1.js
@@ -0,0 +1,34 @@
+﻿ $(function(){
+	khoimonan = $('.nhieumon').isotope({
+  itemSelector: '.motmon',
+  layoutMode: 'masonry'
+});
+ 		khoimonan.imagesLoaded().progress( function() {
+  			khoimonan.isotope('layout');
+	});
+
+ 		$('.tieudect a').click(function(event) {
+ 			dulieu = $(this).data('monan');
+ 			// console.log(dulieu);
+
+ 			khoimonan.isotope({ filter: dulieu });
+
+ 			return false;
+ 		});
+
+ 		// xu ly phan responsive bang jquery
+ 		// tinh ra so div ten .motmon
+ 		// soluongmon = $('.motmon').length;
+ 		// docao = soluongmon*120;
+ 		// console.log(docao);
+
+ 		// $('.noidungct').css({'height':docao})
+
+ 		// xem div motmon co ton tai hay khong
+ 		// if($('.motmon').length){
+ 		// 	console.log('Co div motmon')
+ 		// }
+ 		new WOW().init();
+
+})  
+ 
